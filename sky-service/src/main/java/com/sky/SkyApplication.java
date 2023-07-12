7fc6beeeaa6687c74955395e3db5b61d22150f36
inewyourhelp <1786107845@qq.com>
2023-07-12 11:06:04 +0800

    git init

---
+++ sky-service/src/main/java/com/sky/SkyApplication.java
@@ -0,0 +1,16 @@
+package com.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaolu
+ * @date 2023/7/12 8:54
+ */
+@SpringBootApplication
+public class SkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyApplication.class, args);
+    }
+}
